8936a87d16e1dd240428bd84442c5a6b4cf7793a
Garrett Sharp <sharpgarrett908@gmail.com>
2025-04-06 02:27:57 -0700

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,10 +9,6 @@ public class MainMenu : MonoBehaviour
     // Reference to the background music AudioSource
     public AudioSource backgroundMusic;
 
-    // List of background music tracks
-    public List<AudioClip> musicTracks;
-    private int currentTrackIndex = 0;
-
     // Time to wait before playing the next track
     public float waitTime = 5.0f;
 
@@ -36,44 +32,13 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (backgroundMusic != null && musicTracks.Count > 0)
-        {
-            backgroundMusic.clip = musicTracks[currentTrackIndex];
-            backgroundMusic.Play();
-            backgroundMusic.loop = false;
-        }
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (!backgroundMusic.isPlaying)
-        {
-            interuptStarted = true;
-            StartCoroutine(WaitAndPlayNextTrack());
-        }
-    }
 
-    // Coroutine to wait before playing the next track
-    IEnumerator WaitAndPlayNextTrack()
-    {
-        yield return new WaitForSeconds(waitTime);
-        if (backgroundMusic != null)
-        {
-            yield break;
-        }
-        else
-            PlayNextTrack();
-    }
 
-    // Function to play the next track in the list
-    void PlayNextTrack()
-    {
-        currentTrackIndex = (currentTrackIndex + 1) % musicTracks.Count;
-        backgroundMusic.clip = musicTracks[currentTrackIndex];
-        backgroundMusic.Play();
-        interuptStarted = false;
-    }
+
 
     // Coroutine to handle scene fade transition and loading
     IEnumerator FadeAndLoadScene(string sceneName)
