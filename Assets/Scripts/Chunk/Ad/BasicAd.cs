5dcb64bfec017a1f818755f7d3fe20d7dc7c6549
Garrett Sharp <sharpgarrett908@gmail.com>
2025-04-05 20:15:54 -0700

    Ads

---
+++ Assets/Scripts/Chunk/Ad/BasicAd.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAd : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer videoPlayer;
+    public GameObject rendererGameObject;
+
+    public BasicChunk chunk;
+
+    public bool canContinue = false;
+    public float skipTime = 5.0f;
+
+    private void Awake()
+    {
+        chunk = GetComponent<BasicChunk>();
+        rendererGameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (videoPlayer != null && videoPlayer.isPrepared && videoPlayer.frame >= (long)videoPlayer.frameCount)
+        {
+            rendererGameObject.SetActive(false);
+        }
+    }
+
+    public void OnNextPressed()
+    {
+        if (canContinue)
+        {
+            // Logic to proceed to the next chunk
+            Debug.Log("Proceeding to the next chunk...");
+            chunk.EndChunk();
+        }
+        else
+        {
+            Debug.Log("Please complete the current chunk before proceeding.");
+        }
+    }
+
+    private Coroutine skipCoroutine;
+    public GameObject skipButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skipButton.SetActive(false);
+        skipCoroutine = StartCoroutine(EnableSkipButtonAfterTime());
+    }
+
+    private IEnumerator EnableSkipButtonAfterTime()
+    {
+        yield return new WaitForSeconds(skipTime);
+        skipButton.SetActive(true);
+    }
+
+    public void OnSkipPressed()
+    {
+        if (skipCoroutine != null)
+        {
+            StopCoroutine(skipCoroutine);
+        }
+        videoPlayer.Stop();
+        rendererGameObject.SetActive(false);
+        skipButton.SetActive(false);
+        canContinue = true;
+        OnNextPressed();
+    }
+}
